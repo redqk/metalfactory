4e18382dbf2a3138c5e4062e0dae01b73688099c
unknown <robertred47@hotmail.com>
2024-05-18 19:12:26 +0300

    modificari-minore

---
+++ Metal-Factory/src/components/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="login">
+      <h1>Login</h1>
+      <form @submit.prevent="login">
+        <div>
+          <label for="email">Email:</label>
+          <input type="email" v-model="email" required>
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" v-model="password" required>
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import { mapActions } from 'vuex'
+  
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      ...mapActions(['login']),
+      async login() {
+        await this.login({ email: this.email, password: this.password })
+        this.$router.push('/')
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Adaugă stilurile tale aici */
+  </style>
+  
